e08625aca2defde75740a03f6c902888df55cc0f
rahulsinghbisht-13 <rbisht733@gmail.com>
2024-06-22 02:26:34 +0530

    Styled input elements

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,17 @@
 export default function Input({ label, textarea, ...props }) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
   return (
-    <p>
-      <lable>{label}</lable>
-      {textarea ? <textarea {...props} /> : <input {...props} />}
+    <p className="flex flex-col gap-1 my-4">
+      <lable className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </lable>
+      {textarea ? (
+        <textarea className={classes} {...props} />
+      ) : (
+        <input className={classes} {...props} />
+      )}
     </p>
   );
 }
